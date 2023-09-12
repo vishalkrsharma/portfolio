3896fa94e999ff23a2f2e890bc095b4304f09284
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-09-12 22:26:14 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Vishal Kumar Sharma',
   description: "Explore Vishal Kumar Sharma's portfolio repository, showcasing a collection of projects and achievements.",
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
