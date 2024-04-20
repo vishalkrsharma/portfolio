2f28c78065ba7a8087805795d454000e61c79269
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-04-21 01:24:04 +0530

    fix: hydration fix

--- components/arrow-indicator.tsx
+++ components/arrow-indicator.tsx
@@ -1,8 +1,15 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { FaArrowDown } from 'react-icons/fa';
 
 const ArrowIndicator = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  if (!isMounted) return null;
+
   return (
     typeof window !== 'undefined' &&
     window.innerHeight + window.scrollY < document.body.offsetHeight && <FaArrowDown className='animate-bounce w-8 h-8 mx-auto' />
