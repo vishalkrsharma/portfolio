6d892afc776e69d029961a90c34e3059c115f281
vishalkrsharma <vishalkrsharma1234@gmail.com>
2025-04-30 23:36:19 +0530

    chore: initial commit

--- lib/fonts.ts
+++
@@ -1,7 +0,0 @@
-import { Victor_Mono } from 'next/font/google';
-
-export const victorMono = Victor_Mono({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700'],
-  style: ['normal', 'italic'],
-});
